a95208a85dc63eae117a287e0ab12d4ee4e1eeb4
Jacob Stockton <jacob.stockton@hotmail.com>
2017-06-11 23:48:08 -0400

    major refactor

---
+++ src/main/java/shakespeare/Bot.java
@@ -0,0 +1,150 @@
+package shakespeare;
+
+import twitter4j.*;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import java.util.Arrays;
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.StatusUpdate;
+
+public class Bot {
+
+    Twitter twitter;
+
+    public Bot() {
+        String consumerKey = System.getenv("ConsumerKey");
+        String consumerSecret = System.getenv("ConsumerSecret");
+        String accessToken = System.getenv("AccessToken");
+        String accessTokenSecret = System.getenv("AccessTokenSecret");
+
+        if(consumerKey == null || consumerSecret == null || accessToken == null || accessTokenSecret == null) {
+            throw new RuntimeException("Authentication variables are missing from the environment.");
+        }
+
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(consumerKey)
+                .setOAuthConsumerSecret(consumerSecret)
+                .setOAuthAccessToken(accessToken)
+                .setOAuthAccessTokenSecret(accessTokenSecret);
+
+        cb.setJSONStoreEnabled(true);
+        TwitterFactory tf = new TwitterFactory(cb.build());
+
+        this.twitter = tf.getInstance();
+
+    }
+
+    public void favouriteRecentMentions() throws TwitterException {
+        ResponseList<Status> mentions = twitter.getMentionsTimeline();
+
+        for (Status m : mentions) {
+            try {
+                twitter.createFavorite(m.getId());
+            } catch(TwitterException e) {
+                // There's not much we can do if creating a favourite fails
+                // Sometimes this happens if the user blocked AngryShakespeare :(
+            }
+        }
+    }
+
+    public List<Status> getTweets() {
+
+        List<Status> tweets;
+
+        Query query = new Query(searchFilter());
+        query.count(100);
+
+        try {
+            tweets = twitter.search(query).getTweets();
+        } catch(TwitterException e) {
+            throw new RuntimeException(e);
+        }
+
+        return tweets;
+    }
+
+    public static boolean validTweet(Status tweet) {
+        Long[] bannedIDs = {2993594932L};
+
+        String tweetText = tweet.getText().toLowerCase();
+        String screenName = tweet.getUser().getScreenName().toLowerCase();
+        Long userID = tweet.getUser().getId();
+
+        boolean badTweet = tweetText.contains("-") ||
+                tweetText.contains("\"") || // Likely to be a shakespeare quote
+                tweetText.contains("“") ||
+                tweetText.contains("william") ||
+                tweetText.contains("http://") ||
+                tweetText.contains("https://") ||
+                screenName.contains("shakespeare") || // Likely a Shakespeare twitter account
+                Arrays.asList(bannedIDs).contains(userID);
+
+        return !badTweet;
+
+    }
+
+    public static String getInsult() {
+        String insult = null;
+        try {
+            URL url = new URL("http://www.pangloss.com/seidel/Shaker/index.html");
+
+            URLConnection con = url.openConnection();
+            InputStream is = con.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                if (line.contains("/font")) {
+                    insult = line.replace("</font>", "")
+                            .replace("[", "")
+                            .replace("]", "")
+                            .toLowerCase()
+                            .replace("<br>", "\n");
+                }
+            }
+
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return insult;
+    }
+
+    private static String searchFilter() {
+
+        return "lang:en "
+                + "shakespeare hate OR \"fuck shakespeare\" OR \"shakespeare sucks\" "
+                + "-filter:retweets";
+
+    }
+
+    public Status getLastTweet() throws TwitterException {
+        return twitter.getUserTimeline().get(0);
+    }
+
+    public boolean reply(StatusUpdate tweet, Status targetTweet) {
+
+        tweet.setInReplyToStatusId(targetTweet.getId());
+        try {
+            twitter.updateStatus(tweet);
+        } catch(TwitterException e) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+}
--- src/main/java/shakespeare/Main.java
+++ src/main/java/shakespeare/Main.java
@@ -1,216 +1,67 @@
 package shakespeare;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-
-import java.net.URL;
-import java.net.URLConnection;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Properties;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.StatusUpdate;
-
-import twitter4j.Twitter;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-//
-//import twitter4j.*;
-import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+import static shakespeare.Bot.*;
 
 public class Main {
 
-    public static void main(String[] args) throws MalformedURLException, TwitterException, InterruptedException, IOException {
+    public static void main(String[] args) {
 
-        Twitter twitter = authorize();
+        while (true) {
 
-        URL url = new URL("http://www.pangloss.com/seidel/Shaker/index.html");
+            try {
+                Bot bot = new Bot();
 
-        //favourite recent mentions
-        ResponseList<twitter4j.Status> mentions = twitter.getMentionsTimeline();
-
-        for (int i = 0; i < mentions.size(); i++) {
-
-            if (!mentions.get(i).isFavorited()) {
+                // Favourite recent mentions
                 try {
-                    twitter.createFavorite(mentions.get(i).getId());
+                    bot.favouriteRecentMentions();
                 } catch (TwitterException e) {
-
+                    // TODO: Send email
                 }
 
-            }
-        }
+                for (Status targetTweet : bot.getTweets()) {
 
-        boolean validTweet = false;
-        String tweetText = "";
+                    if (!validTweet(targetTweet)) {
+                        continue;
+                    }
 
-        ArrayList<twitter4j.Status> tweets = searchTweets(searchFilter(), 100, twitter);
+                    if (bot.getLastTweet().getInReplyToScreenName().equals(targetTweet.getUser().getScreenName())) {
+                        continue;
+                    }
 
-        Status tweet = null;
-        String tweetTo = null;
-        for (int i = 0; tweet == null && i < tweets.size(); i++) {
+                    // TODO: Check that minimum tweet time has elapsed
 
-            if (filterTweet(tweets.get(i))) {
+                    String tweetText = null;
 
-                tweet = tweets.get(i);
-                tweetTo = tweets.get(i).getUser().getScreenName();
+                    while (tweetText == null || tweetText.length() > 140) {
+                        String insult = getInsult();
+                        tweetText = ".@" + targetTweet.getUser().getScreenName() + " " + insult;
+                    }
 
-            } else {
-                //System.out.println("@" + tweets.get(i).getUser().getScreenName() + " " + tweets.get(i).getText() + "  //  tweet rejected");
-            }
-        }
+                    StatusUpdate status = new StatusUpdate(tweetText);
 
-        // Get the input stream through URL Connection
-        String insult = null;
-
-        while (!validTweet) {
-            validTweet = false;
-            URLConnection con = url.openConnection();
-            InputStream is = con.getInputStream();
-
-            //generate insult
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String line;
-            while ((line = br.readLine()) != null) {
-                if (line.contains("/font")) {
-                    insult = line.replace("</font>", "").replace("[", "").replace("]", "").toLowerCase().replace("<br>", "\n");
-
-                }
-            }
-
-            //generating tweet text
-            tweetText = "@" + tweetTo + " " + insult;
-
-            if (tweetText.length() <= 140) {
-
-                validTweet = true;
-            }
-        }
-
-        StatusUpdate status = new StatusUpdate(tweetText);
-
-        status.setInReplyToStatusId(tweet.getId());
-
-        boolean duplicate = false;
-        if (twitter.getUserTimeline().size() != 0) {
-
-            if (twitter.getUserTimeline().get(0).getInReplyToScreenName().equals(tweetTo)) {
-                duplicate = true;
-            }
-        }
-
-        if (tweetText.length() <= 140 && !duplicate) {
-            twitter.updateStatus(status);
-
-        }
-
-    }
-
-    public static boolean filterTweet(twitter4j.Status tweet) {
-        Long[] bannedIDs = {2993594932L};
-        ArrayList<Boolean> masterFilter = new ArrayList<>();
-
-        String tweetText = tweet.getText().toLowerCase();
-        String screenName = tweet.getUser().getScreenName().toLowerCase();
-        Long userID = tweet.getUser().getId();
-
-        Boolean[] filters = {
-                tweetText.contains("-"),
-                tweetText.contains("\""), //likely to be a shakespeare quote
-                tweetText.contains("“"),
-                tweetText.contains("william"),
-                tweetText.contains("http://"),
-                tweetText.contains("https://"),
-                screenName.contains("shakespeare"), //likely a Shakespeare twitter account
-                Arrays.asList(bannedIDs).contains(userID)
-
-        };
-
-        masterFilter.add(!Arrays.asList(filters).contains(true));
-
-        return !masterFilter.contains(false);
-
-    }
-
-    public static String searchFilter() {
-
-        return "lang:en "
-                + "shakespeare hate OR \"fuck shakespeare\" OR \"shakespeare sucks\" "
-                + "-filter:retweets";
-
-    }
-
-    public static ArrayList<Status> searchTweets(String searchTerm, int numTweets, twitter4j.Twitter twitter) throws TwitterException {
-
-        ArrayList<twitter4j.Status> tweetsList = new ArrayList<>();
-
-        Query query = new Query(searchTerm);
-        //query.setUntil("2015-07-23");
-
-        query.count(100);
-
-        String tweetText;
-
-        while (tweetsList.size() < numTweets) {
-            QueryResult tweets = twitter.search(query);
-
-            for (twitter4j.Status status : tweets.getTweets()) {
-                tweetText = status.getText();
-                if (tweetsList.contains(status)) {
-                    numTweets = tweetsList.size();
+                    if (bot.reply(status, targetTweet)) {
+                        break;
+                    }
                 }
 
-                if (!tweetText.contains("RT") && !tweetsList.contains(status) && tweetsList.size() < numTweets) {
-                    tweetsList.add(status);
+                // TODO: Sleep
+
+            } catch (Exception e) {
+                // If we're in here then we've probably hit a fatal error
+                // Notify and sleep for 24 hours
+                // TODO Send email
+                try {
+                    TimeUnit.HOURS.sleep(24);
+                } catch (InterruptedException ie) {
+                    throw new RuntimeException(e);
                 }
             }
-
         }
-        return tweetsList;
     }
-
-    public static Twitter authorize() throws FileNotFoundException, IOException {
-
-        String s = System.getProperty("file.separator");
-
-        Properties prop = new Properties();
-        // System.out.println(System.getProperty("user.dir"));
-
-        InputStream input = new FileInputStream("config.properties");
-
-        // load a properties file
-        prop.load(input);
-
-        // get the property value
-        String consumerKey = prop.getProperty("ConsumerKey");
-        String consumerSecret = prop.getProperty("ConsumerSecret");
-        String accessToken = prop.getProperty("AccessToken");
-        String accessTokenSecret = prop.getProperty("AccessTokenSecret");
-
-        input.close();
-
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey(consumerKey)
-                .setOAuthConsumerSecret(consumerSecret)
-                .setOAuthAccessToken(accessToken)
-                .setOAuthAccessTokenSecret(accessTokenSecret);
-        cb.setJSONStoreEnabled(true);
-        TwitterFactory tf = new TwitterFactory(cb.build());
-
-        return tf.getInstance();
-
-    }
-
 }
-
